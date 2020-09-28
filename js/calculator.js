53966f4bde88b4e282537cdf9b15a6afe157904b
dhatGuy <odunsiolakunbi@gmail.com>
2020-09-28 21:54:33 +0100

    refactor calculator and remove coverage folder

--- js/calculator.js
+++ js/calculator.js
@@ -1,18 +1,16 @@
-const calculator = (n1, n2) => {
-  return {
-    add: (n1, n2) => {
-      return n1 + n2;
-    },
-    subtract: (n1, n2) => {
-      return n1 - n2;
-    },
-    divide: (n1, n2) => {
-      return n1 / n2;
-    },
-    multiply: (n1, n2) => {
-      return n1 * n2;
-    },
-  };
+const calculator = {};
+
+calculator.add = (n1, n2) => {
+  return n1 + n2;
+};
+calculator.subtract = (n1, n2) => {
+  return n1 - n2;
+};
+calculator.divide = (n1, n2) => {
+  return n1 / n2;
+};
+calculator.multiply = (n1, n2) => {
+  return n1 * n2;
 };
 
 module.exports = calculator;
